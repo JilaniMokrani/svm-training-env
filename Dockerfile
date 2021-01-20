bfc2063f742cbb9c11c7888387015d52d94bae3f
Jilani Mokrani <jilanimokrani@pop-os.localdomain>
2021-01-20 21:06:37 +0100

    first commit

---
+++ Dockerfile
@@ -0,0 +1,6 @@
+FROM jilani95/data-preprocessing:latest
+RUN pip3 install scikit-learn
+ENV DATA_SOURCE=/data
+ENV MODEL_PATH =/models
+COPY ./main.py .
+CMD python3 main.py && bash
---
+++ main.py
@@ -0,0 +1,23 @@
+import librosa.display
+import scipy.io.wavfile as wavfile
+import numpy
+import os.path
+from os import walk
+from scipy import stats
+import numpy as np
+import librosa 
+import numpy as np
+from scipy.stats import norm
+import pickle
+import seaborn as sns
+import matplotlib.pyplot as plt
+# Import the libraries
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn import svm
+from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.metrics import confusion_matrix
+from pycm import *
+
+
+print("Hello !")
